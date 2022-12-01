935af5deb45eda8171c3e1b6cf9e4477047e2797
Zueon <maineon0701@gmail.com>
2022-12-01 20:50:58 +0900

    working on profile

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -2,35 +2,8 @@ import { Card, Menu, Image } from "antd";
 import Sider from "antd/es/layout/Sider";
 import React from "react";
 
-import {
-  EditOutlined,
-  EllipsisOutlined,
-  SettingOutlined,
-} from "@ant-design/icons";
-import { Avatar } from "antd";
-import {
-  LaptopOutlined,
-  NotificationOutlined,
-  UserOutlined,
-} from "@ant-design/icons";
-const { Meta } = Card;
-const items2 = [UserOutlined, LaptopOutlined, NotificationOutlined].map(
-  (icon, index) => {
-    const key = String(index + 1);
-    return {
-      key: `sub${key}`,
-      icon: React.createElement(icon),
-      label: `subnav ${key}`,
-      children: new Array(4).fill(null).map((_, j) => {
-        const subKey = index * 4 + j + 1;
-        return {
-          key: subKey,
-          label: `option${subKey}`,
-        };
-      }),
-    };
-  }
-);
+import { signout } from "../services/ApiService";
+
 const Profile = () => {
   return (
     <Sider
@@ -38,14 +11,22 @@ const Profile = () => {
       className="site-layout-background"
       style={{ backgroundColor: "#f5f5f5" }}
     >
-      <Card style={{ width: "100%", height: 300 }} bordered={false}></Card>
+      <Card
+        style={{ width: "100%", height: 300 }}
+        bordered={false}
+        title={localStorage.getItem("NICKNAME")}
+      ></Card>
       <Menu
         mode="inline"
         style={{
           height: "100%",
           borderRight: 0,
         }}
-      />
+      >
+        <Menu.Item>item1</Menu.Item>
+        <Menu.Item>item2</Menu.Item>
+        <Menu.Item>item3</Menu.Item>
+      </Menu>
     </Sider>
   );
 };
